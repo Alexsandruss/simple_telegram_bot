d6699e634785470356b1d0498a01bf586d972f37
Alex Andreev <alex97andreev@protonmail.com>
2018-04-16 21:11:48 +0300

    message_handler created

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ import digest
 import locations
 import jsondb
 
-# delays determine how often processes run
+
 delays = jsondb.load_db("db.json")["delays"]
+commands = jsondb.load_db("commands.json")["commands"]
+quotes = jsondb.load_db("quotes.json")["quotes"]
 
 
 # this function update parser's data
@@ -23,12 +25,94 @@ def update_parser(shadow_db, lock, delay):
         time.sleep(delay)
 
 
+def message_handler(incoming_message):
+    global commands
+    global quotes
+    global shadow_db
+    result = None
+    for command in commands.keys():
+        if incoming_message["text"] == command:
+            result = {
+                "method": "send_message",
+                "text": commands[command],
+                "chat_id": incoming_message["chat_id"]
+            }
+    # crypto currencies feature
+    if incoming_message["text"] in ["/bitcoin", "/ethereum", "/ripple"]:
+        result = {
+            "method": "send_message",
+            "text": shadow_db[incoming_message["text"][1:]],
+            "chat_id": incoming_message["chat_id"]
+        }
+    # random quote feature
+    if incoming_message["text"] == "/quote":
+        result = {
+            "method": "send_message",
+            "text": random.choice(quotes),
+            "chat_id": incoming_message["chat_id"]
+        }
+    # throwing dice feature
+    if incoming_message["text"].startswith("/dice"):
+        result = {
+            "method": "send_message",
+            "text": throw_dice(incoming_message["text"]),
+            "chat_id": incoming_message["chat_id"]
+        }
+    # random choice feature
+    if incoming_message["text"].startswith("/random"):
+        random_result = ""
+        try:
+            random_result = random.choice(incoming_message["text"].split(" ")[1:])
+        except:
+            random_result = "Type command correctly"
+        finally:
+            result = {
+                "method": "send_message",
+                "text": random_result,
+                "chat_id": incoming_message["chat_id"]
+            }
+    # days until newyear or summer feature
+    if incoming_message["text"] == "/newyear":
+        result = {
+            "method": "send_message",
+            "text": digest.days_until_newyear(),
+            "chat_id": incoming_message["chat_id"]
+        }
+    if incoming_message["text"] == "/summer":
+        result = {
+            "method": "send_message",
+            "text": digest.days_until_summer(),
+            "chat_id": incoming_message["chat_id"]
+        }
+    # locations feature
+    if incoming_message["text"].startswith("/where"):
+        try:
+            location = incoming_message["text"].split(" ")[1]
+            result = {
+                "method": "send_location",
+                "coordinates": locations.get_coordinates(location),
+                "chat_id": incoming_message["chat_id"]
+            }
+        except:
+            result = {
+                "method": "send_message",
+                "text": "Type command correctly",
+                "chat_id": incoming_message["chat_id"]
+            }
+    # chat id getter
+    if incoming_message["text"] == "/chat_id":
+        result = {
+            "method": "send_message",
+            "text": incoming_message["chat_id"],
+            "chat_id": incoming_message["chat_id"]
+        }
+    return result
+
+
 # this function collects updates for bot from telegram and response on it
 def bot_processor(shadow_db, lock, delay):
     # load dictionaries that stores token, last update's id, command, quotes etc.
     db = jsondb.load_db("db.json")
-    commands = jsondb.load_db("commands.json")["commands"]
-    quotes = jsondb.load_db("quotes.json")["quotes"]
     bot = Bot(token=db["token"], keep_log=True)
     bot.last_checked_update_id = db["last_checked_update_id"]
     while True:
@@ -37,47 +121,17 @@ def bot_processor(shadow_db, lock, delay):
         for message in messages:
             if round(time.time()) - message["date"] <= db["max_time_diff"]:
                 try:
-                    text = message["text"]
-                    chat_id = message["chat"]["id"]
+                    incoming_message = {"text": message["text"], "chat_id": message["chat"]["id"]}
                 # some messages have not text (stickers, files etc)
                 except:
                     continue
-                for command in commands.keys():
-                    if text == command:
-                        bot.send_message(chat_id, commands[command])
-                # crypto currencies feature
-                if text == "/bitcoin" or text == "/ethereum" or text == "/ripple":
-                    bot.send_message(chat_id, shadow_db[text[1:]])
-                # random quote feature
-                if text == "/quote":
-                    bot.send_message(chat_id, random.choice(quotes))
-                # throwing dice feature
-                if text.startswith("/dice"):
-                    bot.send_message(chat_id, throw_dice(text))
-                # random choice feature
-                if text.startswith("/random"):
-                    outgoing_message = ""
-                    try:
-                        outgoing_message = random.choice(text.split(" ")[1:])
-                    except:
-                        outgoing_message = "Type command correctly"
-                    finally:
-                        bot.send_message(chat_id, outgoing_message)
-                # days until newyear or summer feature
-                if text == "/newyear":
-                    bot.send_message(chat_id, digest.days_until_newyear())
-                if text == "/summer":
-                    bot.send_message(chat_id, digest.days_until_summer())
-                # locations feature
-                if text.startswith("/where"):
-                    try:
-                        location = text.split(" ")[1]
-                        bot.send_location(chat_id, locations.get_coordinates(location))
-                    except:
-                        bot.send_message(chat_id, "Type command correctly")
-                # chat id getter
-                if text == "/chat_id":
-                    bot.send_message(chat_id, chat_id)
+                outgoing_message = message_handler(incoming_message)
+                if outgoing_message is None:
+                    continue
+                elif outgoing_message["method"] == "send_message":
+                    bot.send_message(outgoing_message["chat_id"], outgoing_message["text"])
+                elif outgoing_message["method"] == "send_location":
+                    bot.send_location(outgoing_message["chat_id"], outgoing_message["coordinates"])
         db["last_checked_update_id"] = bot.last_checked_update_id
         jsondb.save_db("db.json", db)
         lock.release()
